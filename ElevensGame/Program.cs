3957fe570b6a9e3f05ff65089ba76e4773785b3e
Isadore <remyrascal@live.com>
2025-05-22 03:09:23 -0400

    created method to check if user has selected a valid card combination

--- ElevensGame/Program.cs
+++ ElevensGame/Program.cs
@@ -3,20 +3,37 @@
     private int pairvalue;
     private Deck deck;
     private List<Card> CardTable;
-    private List<int> SelectedCards;
+    private List<(Card, int)> SelectedCards; // (Card, CardTable Index)
     public Game(int pairvalue = 11)
     {
         this.pairvalue = pairvalue;
     }
 
     public void GameLoop() { }
-    public void DisplayTable() { }
+    public void DisplayTable()
+    {
+        
+    }
     public bool CheckSelectedCards()
     {
         if (SelectedCards.Count == 2)
         {
-            CardTable.ElementAt(SelectedCards.First());
-        } else {}
+            int ranksum = SelectedCards[0].Item1.getRankNum() + SelectedCards[1].Item1.getRankNum();
+            return ranksum == pairvalue;
+        }
+        if (SelectedCards.Count == 3)
+        {
+            bool jack = false;
+            bool queen = false;
+            bool king = false;
+            foreach ((Card, int) ci in SelectedCards)
+            {
+                if (ci.Item1.Rank == Rank.Jack) jack = true;
+                if (ci.Item1.Rank == Rank.Queen) queen = true;
+                if (ci.Item1.Rank == Rank.Queen) queen = true;
+            }
+            return jack && queen && king;
+        }
         return false;
     }
     
